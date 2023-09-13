d408fdd9a98de9fc73af08abd3d0706ecb7e72ef
Rafael Araujo <kasim.85.ra@gmail.com>
2023-09-12 21:19:11 -0300

    inicio de app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import injectContext from "./store/appContext";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <ScrollToTop>
+          <Routes>
+            {/* <Route path="/" element={<Home />} />
+            <Route path="*" element={<h1>Not found!</h1>} /> */}
+          </Routes>
+        </ScrollToTop>
+      </BrowserRouter>
+    </>
+
   );
 }
 
-export default App;
+export default injectContext(App);
---
+++ src/store/flux.js
@@ -0,0 +1,15 @@
+import { clippingParents } from "@popperjs/core";
+
+const getState = ({ getStore, getActions, setStore }) => {
+  return {
+    store: {
+
+    },
+    actions: {
+
+
+    },
+  };
+};
+
+export default getState;
