f81177b86c7af95239d7571cc65560a0f75feb49
Rafael Araujo <kasim.85.ra@gmail.com>
2023-09-15 18:30:25 -0300

    agregando navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+export const MenuNavbar = () => {
+  return (
+    <nav className=" container  navbar navbar-expand-lg bg-body-tertiary mt-2">
+      <div className="container-fluid">
+        <image src="" className="navbar-brand">
+          Logo
+        </image>
+        <h4>Sistema de Matricula Escolar</h4>
+        <button
+          className="navbar-toggler"
+          disabled="true"
+          type="button"
+          data-bs-toggle="dopdown"
+          aria-expanded="false"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <button
+                className="btn btn- ligth dropdown-toggle"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Menu
+              </button>
+              <ul className="dropdown-menu">
+                <li>Apoderado Académico</li>
+                <li>Apoderado Financiero</li>
+                <li>Mis Datos</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  );
+};
